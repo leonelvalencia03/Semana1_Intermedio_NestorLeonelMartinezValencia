398e3565a35cef74a6070ac1d2535e240b175396
leonelvalencia03 <nestor.martinez23@itca.edu.sv>
2025-09-24 16:06:05 -0600

    wip ejercicio1

---
+++ src/main/java/org/example/ejercicio1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        //Ejercicio de banco
+
+        System.out.println("==================================================");
+        System.out.println("                   BANCO LEONEL                   ");
+        System.out.println("==================================================");
+
+        System.out.println("1- Consultar saldo ");
+        System.out.println("2- Depositar dinero ");
+        System.out.println("3- Retirar dinero ");
+        System.out.println("4- Salir ");
+
+    }
+
+}
