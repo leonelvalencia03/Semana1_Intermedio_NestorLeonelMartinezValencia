99b401d9f6bac68a8e88d4e140e377cb1bf728b9
leonel <nestor.martinez23@itca.edu.sv>
2025-09-24 20:31:04 -0600

    wip ejercicio2

---
+++ src/main/java/org/example/ejercicio2.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Scanner;
+import java.math.MathContext;
+
+public class ejercicio2 {
+    public static void main(String[] args){
+        //Ejercicio No2: Calculadora
+
+        //Declaracion de la clase Scanner para poder introducir valores
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("======================================");
+        System.out.printf("       Calculadora Avanzada           ");
+        System.out.printf("======================================");
+        System.out.printf("1- Suma");
+        System.out.printf("2- Resta");
+        System.out.printf("3- Multiplicación");
+        System.out.printf("4- Potencia");
+        System.out.printf("5- Raíz cuadrada");
+        System.out.printf("Escoga una opción: ");
+        int op = sc.nextInt();
+
+
+
+    }
+}
