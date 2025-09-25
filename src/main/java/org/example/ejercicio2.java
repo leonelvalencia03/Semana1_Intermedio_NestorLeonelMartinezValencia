df5fb5af44211dabcfc4d8d398d919b500e11f57
leonel <nestor.martinez23@itca.edu.sv>
2025-09-24 21:14:19 -0600

    wip ejercicio2

--- src/main/java/org/example/ejercicio2.java
+++ src/main/java/org/example/ejercicio2.java
@@ -5,22 +5,34 @@ import java.math.MathContext;
 public class ejercicio2 {
     public static void main(String[] args){
         //Ejercicio No2: Calculadora
-
+        //Declaración de variables
+        double n1, n2;
         //Declaracion de la clase Scanner para poder introducir valores
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("======================================");
-        System.out.printf("       Calculadora Avanzada           ");
-        System.out.printf("======================================");
-        System.out.printf("1- Suma");
-        System.out.printf("2- Resta");
-        System.out.printf("3- Multiplicación");
-        System.out.printf("4- Potencia");
-        System.out.printf("5- Raíz cuadrada");
+        System.out.printf("======================================\n");
+        System.out.printf("       Calculadora Avanzada           \n");
+        System.out.printf("======================================\n");
+        System.out.printf("1- Suma\n");
+        System.out.printf("2- Resta\n");
+        System.out.printf("3- Multiplicación\n");
+        System.out.printf("4- Potencia\n");
+        System.out.printf("5- Raíz cuadrada\n");
         System.out.printf("Escoga una opción: ");
         int op = sc.nextInt();
 
+        switch (op){
+            case 1:
+                System.out.printf("\n\nIngrese valor de número 1: ");
+                n1 = sc.nextDouble();
+                System.out.printf("\nIngrese valor de número 2: ");
+                n2 = sc.nextDouble();
 
+                System.out.printf("\n\n%.2f + %.2f = %.2f",n1,n2, n1+n2);
+                break;
+            default:
+                System.out.printf("ERROR: Seleccione una opción válida");
+        }
 
     }
 }
